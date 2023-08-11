28b009e59538177dd5c1e74b0c3dc34fd418c4c6
abhinavkumarlko <103668170+abhinavkumarlko@users.noreply.github.com>
2023-08-12 00:56:28 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mynodeapp
+# iacsdrepomarch23
+This repo contains source code for NodeJS Web app
---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Hello World"
+cat > tfl1 <<EOF
+
+info code info 
+info code info
+info code info
+
+EOF 
